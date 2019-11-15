28836dd0b28934024304daa2e5f1c2d7b86acf0d
berberman <1793913507@qq.com>
2019-11-15 11:51:49 +0800

    clean up id

--- src/test/kotlin/pid/PID.kt
+++ src/test/kotlin/pid/PID.kt
@@ -1,7 +1,6 @@
 package pid
 
 import cn.berberman.parseck.*
-import cn.berberman.parseck.dt.Left
 import cn.berberman.parseck.parser.*
 import org.mechdancer.remote.presets.RemoteHub
 import org.mechdancer.remote.presets.remoteHub
@@ -18,7 +17,11 @@ data class PID(
     val deadArea: Double
 )
 
-var pid = PID(.0, .0, .0, .0, .0)
+var pidS = Array(3){PID(.0, .0, .0, .0, .0)}
+
+var i = 0
+
+fun currentPID()= pidS[i]
 
 val jsEngine = ScriptEngineManager().getEngineByName("nashorn")
 val operation = listOf("kp", "ki", "kd", "ia", "da").sortedByDescending { it.length }.map { string(it) }.choice()
@@ -27,12 +30,12 @@ val expr = operation bind { op ->
     lexeme {
         arg bind { num ->
             returnP {
-                pid = when (op) {
-                    "kp"  -> pid.copy(k = num)
-                    "ki" -> pid.copy(ki = num)
-                    "kd" -> pid.copy(kd = num)
-                    "ia" -> pid.copy(integrateArea = num)
-                    "da" -> pid.copy(deadArea = num)
+                pidS[i] = when (op) {
+                    "kp" -> currentPID().copy(k = num)
+                    "ki" -> currentPID().copy(ki = num)
+                    "kd" -> currentPID().copy(kd = num)
+                    "ia" -> currentPID().copy(integrateArea = num)
+                    "da" -> currentPID().copy(deadArea = num)
                     else -> throw Unknown
                 }
             }
@@ -45,52 +48,53 @@ val remote = remoteHub("pwh").also {
 }
 
 fun RemoteHub.sendPID(pid: PID, id: Int, reset: Boolean = true) =
-        broadcast(object : Command {
-            override val id: Byte = 9
-        }, ByteArrayOutputStream().let {
-            DataOutputStream(it).run {
-                with(pid) {
-                    writeInt(id)
-                    writeDouble(k)
-                    writeDouble(ki)
-                    writeDouble(kd)
-                    writeDouble(integrateArea)
-                    writeDouble(deadArea)
-                    writeBoolean(reset)
-                }
-                it.toByteArray()
+    broadcast(object : Command {
+        override val id: Byte = 9
+    }, ByteArrayOutputStream().let {
+        DataOutputStream(it).run {
+            with(pid) {
+                writeInt(id)
+                writeDouble(k)
+                writeDouble(ki)
+                writeDouble(kd)
+                writeDouble(integrateArea)
+                writeDouble(deadArea)
+                writeBoolean(reset)
             }
-        })
+            it.toByteArray()
+        }
+    })
 
 fun RemoteHub.sendDouble(double: Double, id: Int) =
-        broadcast(object : Command {
-            override val id: Byte = 32
-        }, ByteArrayOutputStream().let {
-            DataOutputStream(it).run {
-                writeInt(id)
-                writeDouble(double)
-                it.toByteArray()
-            }
-        })
-fun RemoteHub.sendPosition(x:Double, y:Double,w:Double) =
-        broadcast(object : Command {
-            override val id: Byte = 10
-        }, ByteArrayOutputStream().let {
-            DataOutputStream(it).run {
-                writeDouble(x)
-                writeDouble(y)
-                writeDouble(w)
-                it.toByteArray()
-            }
-        })
+    broadcast(object : Command {
+        override val id: Byte = 32
+    }, ByteArrayOutputStream().let {
+        DataOutputStream(it).run {
+            writeInt(id)
+            writeDouble(double)
+            it.toByteArray()
+        }
+    })
 
-val parser = string("pid") * space * expr.some()
+fun RemoteHub.sendPosition(x: Double, y: Double, w: Double) =
+    broadcast(object : Command {
+        override val id: Byte = 10
+    }, ByteArrayOutputStream().let {
+        DataOutputStream(it).run {
+            writeDouble(x)
+            writeDouble(y)
+            writeDouble(w)
+            it.toByteArray()
+        }
+    })
+
+val parser = int bind { i = it; space * expr.some() }
 
 fun main() {
     while (true) {
-        //pid.getPid ki ... k ... (at least one term)
+        // ki ... k ... (at least one term)
         parser(readLine()!!)/*.let { if (it is Left) println(it.value) }*/
-        println(pid)
-        remote.sendPID(pid,0)
+        println(currentPID())
+        remote.sendPID(currentPID(), i)
     }
 }
